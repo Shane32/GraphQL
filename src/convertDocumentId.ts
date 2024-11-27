a6b6639e88009979973cc5471e8206a6f0ea347a
Shane32 <shane@acdmail.com>
2024-11-27 10:05:44 -0500

    update

--- src/convertDocumentId.ts
+++ src/convertDocumentId.ts
@@ -1,4 +1,5 @@
 export function convertDocumentId(documentId: string) {
-  // if the documentId is 64 hexadecimal characters, it's probably a sha256 hash
-  return documentId.match(/^[0-9a-f]{64}$/i) ? "sha256:" + documentId : documentId;
+  // if the documentId is 64 lowercase hexadecimal characters, it's probably a standardized
+  // sha256 hash and should be prefixed with "sha256:" to indicate that
+  return documentId.match(/^[0-9a-f]{64}$/) ? "sha256:" + documentId : documentId;
 }
