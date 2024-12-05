f6e7ed6a6c327d59c9b59831f7151c7b237a09f9
Shane32 <shane@acdmail.com>
2024-12-05 16:39:15 -0500

    Use hash as documentId

--- src/convertDocumentId.ts
+++
@@ -1,5 +0,0 @@
-export function convertDocumentId(documentId: string) {
-  // if the documentId is 64 lowercase hexadecimal characters, it's probably a standardized
-  // sha256 hash and should be prefixed with "sha256:" to indicate that
-  return documentId.match(/^[0-9a-f]{64}$/) ? "sha256:" + documentId : documentId;
-}
